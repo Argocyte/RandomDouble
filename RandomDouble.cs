37f61fe3d855464c6152cfd80e403bd4f8302e79
Argocyte <35248818+Argocyte@users.noreply.github.com>
2020-02-18 22:55:37 +0000

    Update RandomDouble.cs
    Added functionality and tidied the code.

--- RandomDouble.cs
+++ RandomDouble.cs
@@ -7,6 +7,35 @@ namespace RandomDouble
     /// </summary>
     public class RandomDouble
     {
+        private readonly Random random;
+
+        /// <summary>
+        /// initiates RandomDouble with a time-dependant seed value.
+        /// </summary>
+        public RandomDouble()
+        {
+            random = new Random();
+        }
+        /// <summary>
+        /// inititates RandomDouble with a seed value.
+        /// </summary>
+        public RandomDouble(int seed)
+        {
+            random = new Random(seed);
+        }
+
+        /// <summary>
+        /// Returns a random double between 0 and max.
+        /// </summary>
+        /// <param name="max">The maximum value</param>
+        /// <returns>Returns a random double between max and min</returns>
+        public double Next(double max)
+        {
+            // NextDouble gives random number between 0.0 and 1.0.
+            // Range is created by multiplying with max - min, 
+            // then added to the min to create the true range.
+            return random.NextDouble() * max;
+        }
         /// <summary>
         /// Returns a random double between max and min.
         /// </summary>
@@ -15,10 +44,10 @@ namespace RandomDouble
         /// <returns>Returns a random double between max and min</returns>
         public double Next(double min, double max)
         {
-            Random rand = new Random();
-            double Value;
-            Value = min + (rand.NextDouble() * (max - min)); // NextDouble gives random number between 0.0 and 1.0, range is created by multiplying with max - min, then added to the min to create the true range.
-            return Value;
+            // NextDouble gives random number between 0.0 and 1.0.
+            // Range is created by multiplying with max - min, 
+            // then added to the min to create the true range.
+            return min + (random.NextDouble() * (max - min));
         }
     }
 }
